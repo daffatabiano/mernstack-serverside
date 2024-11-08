26b27ff3a897e032d9b49399c85a6676787b4890
unknown <daffatabianoo@gmail.com>
2024-11-08 16:32:51 +0700

    change for trying fixing

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ db.once('open', () => {
 });
 
 app.use(
-  cors({
-    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
+  cors()
+  //   {
+  //   origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // }
 );
 
 app.use(express.json());
