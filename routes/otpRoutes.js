18ce73ec6bc0f4815b24420797421e039a4a467d
unknown <daffatabianoo@gmail.com>
2024-12-18 07:05:19 +0700

    feat(routes): otpRoutes added for routing rest API

---
+++ routes/otpRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { sendOTP, verifyOTP } from '../controllers/verificationController.js';
+
+const router = express.Router();
+
+router.post('/api/v1/send-otp', sendOTP);
+router.post('/api/v1/verify-otp', verifyOTP);
+
+export default router;
