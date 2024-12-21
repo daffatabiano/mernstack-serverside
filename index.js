98c2315c4c389fc647d91c99e6e5a0b2c5e1517c
unknown <daffatabianoo@gmail.com>
2024-12-21 19:18:52 +0700

    fix temp

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import otpRoutes from './routes/otpRoutes.js';
+// import otpRoutes from './routes/otpRoutes.js';
 
 dotenv.config();
 
@@ -20,15 +20,7 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.use(
-  cors()
-  //   {
-  //   origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
-  //   credentials: true,
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  // }
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
-app.use(otpRoutes);
+// app.use(otpRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
