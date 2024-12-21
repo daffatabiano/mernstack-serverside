fc28b99caa1437b6455fbe1e1ac922bba59dc7f7
unknown <daffatabianoo@gmail.com>
2024-12-21 19:29:30 +0700

    feat(models): add customer models features "

---
+++ models/customerModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  voucher: {
+    type: Array,
+  },
+  order: {
+    type: Array,
+  },
+});
+
+export default mongoose.model('Customer', customerSchema);
