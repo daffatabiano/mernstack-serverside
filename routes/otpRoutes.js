54a552bc2c4e52c0ac41cc24b1c8b68a6b3b7968
unknown <daffatabianoo@gmail.com>
2024-12-21 19:20:54 +0700

    routes(otpRoutes): changing for updating ...

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { sendOTP, verifyOTP } from '../controllers/verificationController.js';
+// import express from 'express';
+// import { sendOTP, verifyOTP } from '../controllers/verificationController.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/api/v1/send-otp', sendOTP);
-router.post('/api/v1/verify-otp', verifyOTP);
+// router.post('/api/v1/send-otp', sendOTP);
+// router.post('/api/v1/verify-otp', verifyOTP);
 
-export default router;
+// export default router;
