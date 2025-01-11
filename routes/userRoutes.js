c351cdd820892863b6def88c3983c0726f9e5890
unknown <daffatabianoo@gmail.com>
2025-01-11 14:12:45 +0700

    feat(routes):userRoutes added sending voucher to customer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteUser,
   login,
   getAllUsers,
+  giftVoucherCustomer,
 } from '../controllers/userController.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
@@ -15,6 +16,7 @@ router.post('/api/v1/auth/register', createUser);
 router.post('/api/v1/auth/login', login);
 router.get('/api/v1/users', verifyToken, getAllUsers);
 router.get('/api/v1/user/:id', verifyToken, getUserById);
+router.post('/api/v1/gift-voucher', verifyToken, giftVoucherCustomer);
 router.put('/api/v1/user/:id', verifyToken, updateUser);
 router.delete('/api/v1/user/:id', verifyToken, deleteUser);
 
