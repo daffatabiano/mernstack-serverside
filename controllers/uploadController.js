1ff2464df3f8e0a3e0a9a3f82bc875902121142e
unknown <daffatabianoo@gmail.com>
2025-01-11 14:11:46 +0700

    feat(controller): upload features update

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,9 +1,13 @@
 import path from 'path';
 import fs from 'fs';
+import File from '../models/uploadModel.js';
 
+// Fungsi untuk mengunggah file
 export const uploadFiles = async (req, res) => {
   const { file } = req;
+
   try {
+    // Simpan informasi file ke database
     const fileData = new File({
       fileName: file.originalname,
       filePath: file.path,
@@ -12,38 +16,58 @@ export const uploadFiles = async (req, res) => {
     });
 
     const savedFile = await fileData.save();
-    res
-      .status(200)
-      .json({ message: 'File uploaded successfully', file: savedFile });
+
+    res.status(200).json({
+      message: 'File uploaded successfully',
+      file: savedFile,
+    });
   } catch (err) {
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
+// Fungsi untuk mengambil file
 export const getFile = async (req, res) => {
   try {
-    const file = await File.find({ fileName: req.params.fileName });
+    const file = await File.findOne({ fileName: req.params.fileName });
 
     if (!file) {
       return res.status(404).json({ message: 'File not found' });
     }
 
+    const stream = fs.createReadStream(file.filePath);
+    stream.on('error', (error) => {
+      console.error(error);
+      return res.status(500).json({ message: 'Error reading file' });
+    });
+
     res.setHeader('Content-Type', file.mimetype);
-    res.send(fs.createReadStream(file.filePath));
-    res.status(200).json({ message: 'File fetched successfully', file });
+    stream.pipe(res);
   } catch (err) {
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
 
+// Fungsi untuk menghapus file
 export const deleteFile = async (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(process.cwd(), '../uploads', fileName);
+  try {
+    const file = await File.findOne({ fileName: req.params.fileName });
 
-  if (fs.existsSync(filePath)) {
-    fs.unlinkSync(filePath);
-    return res.status(200).json({ message: 'File deleted successfully' });
+    if (!file) {
+      return res.status(404).json({ message: 'File not found' });
+    }
+
+    if (fs.existsSync(file.filePath)) {
+      fs.unlinkSync(file.filePath);
+    }
+
+    await File.deleteOne({ _id: file._id });
+
+    res.status(200).json({ message: 'File deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
   }
-
-  return res.status(404).json({ message: 'File not found' });
 };
