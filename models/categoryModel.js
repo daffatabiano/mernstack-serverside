0aae65705a54855029567cac42ad5c02b82ad8cc
unknown <daffatabianoo@gmail.com>
2025-02-08 12:38:09 +0700

    feat(models): category models

---
+++ models/categoryModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+export default mongoose.model('Category', categorySchema);
