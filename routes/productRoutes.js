3bb2638209e3572184ad38bf70c3b823df0e3924
unknown <daffatabianoo@gmail.com>
2025-02-10 20:31:16 +0700

    feat(routes): feat product routs

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import midTransClient from 'midtrans-client';
 import {
   createProduct,
   deleteProduct,
@@ -11,11 +10,6 @@ import { adminPermission } from '../middlewares/adminPermission.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
-
-let snap = new midTransClient.Snap({
-  isProduction: false,
-  serverKey: 'SB-Mid-server-xrqyBbFmyc1Oco4RkTstzmbj',
-});
 
 router.get('/api/v1/products', getProducts);
 router.get(
@@ -32,35 +26,5 @@ router.delete(
   adminPermission,
   deleteProduct
 );
-router.post('/api/v1/midtrans', async (req, res) => {
-  let { id, amount, firstName, lastName, email, phone } = req.body;
-
-  if (!id || !amount) {
-    return res
-      .status(400)
-      .json({ message: 'All required fields must be provided' });
-  }
-
-  try {
-    const token = await snap.createTransactionToken({
-      transaction_details: {
-        order_id: id,
-        gross_amount: amount,
-      },
-      credit_card: {
-        secure: true,
-      },
-      customer_details: {
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        phone: phone,
-      },
-    });
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 export default router;
