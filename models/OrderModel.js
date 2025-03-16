88df9c236d3591668f4f9624b0771a921578b2ae
unknown <daffatabianoo@gmail.com>
2025-03-16 14:53:27 +0700

    update fix

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -53,11 +53,9 @@ const OrderSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
     },
     email: {
       type: String,
-      required: true,
     },
     amount: {
       type: Number,
